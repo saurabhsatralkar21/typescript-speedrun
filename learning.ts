dd46921a623d90cb86a20d37d86b141129d713cd
Saurabh S <saurabh.satralkar7@gmail.com>
2023-01-18 22:53:34 +0100

    Typescript catches erros

--- learning.ts
+++ learning.ts
@@ -7,3 +7,20 @@ const user: User = {
     id: 7,
     name: "John Doe",
 }
+
+//Functions
+
+function getAdminUser(): User { // :User means return something as an interface
+    return ({
+        id: 1,
+        name: "something",
+    });
+}
+
+user.location; // Error message
+
+//Catch spelling mistakes
+
+const someText = "Something is in here";
+someText.toLocaleLowerCase();
+someText.tolocaleLowerCase(); // Typescript caught spelling error
